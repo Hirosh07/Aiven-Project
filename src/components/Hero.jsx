da47207917b3ca828244cea42766b96537becda1
Hirosh07 <h97480448@gmail.com>
2025-11-16 17:31:44 +0530

    bluid fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import CtaButton from "./CtaButton";
-import VideoBtn from "./VideoBtn";
 import img from "../img/hero-img.svg";
 // aos
 import AOS from "aos";
